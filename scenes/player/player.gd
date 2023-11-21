b637359ab84152f9b38e9b967ee55e49090bd9f9
Omar Emad <2001omaremad@gmail.com>
2023-11-21 12:59:32 +0200

    signals basics

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
-var speed: int = 500
+const speed: int = 500
+var can_laser:bool = true
+var can_grenade:bool = true
+
 
 func _process(_delta):
 	#movement
@@ -8,8 +11,20 @@ func _process(_delta):
 	velocity = direction * speed
 	move_and_slide()
 	#shooting
-	if Input.is_action_just_pressed("primary action"):
+	if Input.is_action_just_pressed("primary action") and can_laser:
 		print("pew")
-	#gernade
-	if Input.is_action_just_pressed("secondary action"):
+		can_laser = false
+		$LaserTimer.start()
+	#grenade
+	if Input.is_action_just_pressed("secondary action") and can_grenade:
 		print("kaboom")
+		can_grenade = false
+		$GrenadeTimer.start()
+
+
+func _on_laser_timer_timeout():
+	can_laser = true # Replace with function body.
+
+
+func _on_grenade_timer_timeout():
+	can_grenade = true
