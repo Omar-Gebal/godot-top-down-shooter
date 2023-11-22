1d308122ffbdee873406256cfa3e007709e778dc
Omar Emad <2001omaremad@gmail.com>
2023-11-22 13:24:51 +0200

    particles

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -24,6 +24,7 @@ func _process(_delta):
 		var player_direction = (get_global_mouse_position() - position).normalized()
 		laser.emit(selected_marker.global_position, player_direction)
 		$LaserTimer.start()
+		$GPUParticles2D.emitting = true
 	#grenade
 	if Input.is_action_just_pressed("secondary action") and can_grenade:
 		can_grenade = false
