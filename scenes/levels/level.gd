43a26c9b4781cb69fcc290e2c982fa5836bb0b15
Omar Emad <2001omaremad@gmail.com>
2023-11-27 17:37:58 +0200

    coloring ui

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -10,11 +10,11 @@ func _on_player_laser(pos, direction):
 	laser.direction = direction
 	laser.rotation_degrees = rad_to_deg(direction.angle()) + 90 
 	$Projectiles.add_child(laser)
-	$UI.update_laser_text()
+	$UI.update_laser()
 	
 func _on_player_grenade(pos, direction):
 	var grenade = grenade_scene.instantiate() as RigidBody2D
 	grenade.position = pos
 	grenade.linear_velocity = direction * grenade.speed
 	$Projectiles.add_child(grenade)
-	$UI.update_grenade_text()
+	$UI.update_grenade()
--- scenes/user interface/ui.gd
+++ scenes/user interface/ui.gd
@@ -1,14 +1,30 @@
 extends CanvasLayer
 
+#colors
+@export var green : Color = Color("6bbfa3")
+@export var red : Color = Color(0.9,0.1,0.1,1)
+
 @onready var laser_label: Label = $BulletCounter/Label
 @onready var grenade_label : Label = $GrenadeCounter/Label
+@onready var laser_icon : TextureRect = $BulletCounter/TextureRect
+@onready var grenade_icon : TextureRect = $GrenadeCounter/TextureRect
 
 func _ready():
-	update_laser_text()
-	update_grenade_text()
+	update_laser()
+	update_grenade()
 
-func update_laser_text():
+func update_laser():
 	laser_label.text = str(Globals.laser_amount)
+	update_color(Globals.laser_amount, laser_label, laser_icon)
 
-func update_grenade_text():
+func update_grenade():
 	grenade_label.text = str(Globals.grenade_amount)
+	update_color(Globals.grenade_amount, grenade_label, grenade_icon)
+	
+func update_color(amount: int, label : Label, icon : TextureRect):
+	if amount > 0:
+		label.modulate = green
+		icon.modulate = green
+	else:
+		label.modulate = red
+		icon.modulate = red
